5a02f5118be8361f5aed41b191661b7a742a4598
Daniel Poe <thed4nm4n@gmail.com>
2022-02-16 11:17:47 -0500

    Moved from Playgrounds app to Project

--- Cards App/ContentView.swift
+++ Cards App/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+    
+    // Stores the card currently displayed
+    @State private var currentCard: Card
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    // Overrrides startup code to initialize all Image objects
+    init() {
+        
+        // See Data.swift
+        generateImages()
+        
+        // Sets initial card to display
+        self.currentCard = deckOfCards[0]
+    }
+
+    var body: some View {
+        VStack {
+            
+            // Card as image
+            Button(action: {
+                // Gets index of the card on the display
+                let currentIndex = deckOfCards.firstIndex(of: currentCard)!
+                
+                // End of deck loopback
+                if currentIndex == 51 {
+                    
+                    // If at the 52nd card, move back to index 0 (1st card)
+                    currentCard = deckOfCards[0]
+
+                } else {
+                    
+                    // Increments the card shown by moving forward in the array's index
+                    currentCard = deckOfCards[currentIndex + 1]
+                }
+
+            }){
+                currentCard.image
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            // Make it easier to read
+            .padding()
+            .font(.title2)
+        }
     }
 }
