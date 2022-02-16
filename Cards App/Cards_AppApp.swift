bbd4c8665b6c429ee1c7447ed7e146b9df3fd2b4
Daniel Poe <thed4nm4n@gmail.com>
2022-02-16 11:13:57 -0500

    Initial Commit

---
+++ Cards App/Cards_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cards_AppApp.swift
+//  Cards App
+//
+//  Created by Daniel Jaye Poe (Student) on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Cards_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
